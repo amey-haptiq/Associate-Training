6819cda47f570edff8e197a00ac657249072999f
amey kulkarni <amey.kulkarni@haptiq.com>
2025-08-05 19:58:26 +0530

    untracking BlogApi

--- Java/BlogApi/src/main/java/com/example/blogApiTrial/BlogApi/repository/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.blogApiTrial.BlogApi.repository;
-
-import com.example.blogApiTrial.BlogApi.model.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment,Integer> {
-}
--- Java/BlogApi/src/main/java/com/example/blogApiTrial/BlogApi/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.blogApiTrial.BlogApi.repository;
-
-import com.example.blogApiTrial.BlogApi.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Integer> {
-    User findByUserName(String userName);
-}
